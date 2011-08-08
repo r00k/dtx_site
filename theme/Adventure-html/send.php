f23bb1204c4d42ea61cd0717b9388aacd463b126
Ben Orenstein <ben.orenstein@gmail.com>
2011-08-07 21:54:59 -0400

    Initial theme dump

---
+++ theme/Adventure-html/send.php
@@ -0,0 +1,14 @@
+﻿<?php
+	if($_POST){
+		$to = 'example@example.com';/*Put Yuur Email Adress Here*/
+		$subject = $_POST['subject'];
+		$from_name = $_POST['name'];
+		$from_email = $_POST['email'];
+		$message = $_POST['message'];
+		$header = "From: $from_name <$from_email>";
+		mail($to, $subject, $message, $header);
+					
+				
+			
+	}
+?>
